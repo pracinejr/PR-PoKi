6a0369e9389b1309c26549e97ee0a67b887a30c5
Peter Racine Jr <peterracinejr@gmail.com>
2021-09-02 13:39:12 -0500

    completed all queries from 1 to 7

---
+++ SQLQuery2.sql
@@ -0,0 +1,25 @@
+﻿--SELECT * 
+--FROM Grade;
+
+--SELECT DISTINCT e.Name
+--FROM PoemEmotion pe
+--	LEFT JOIN Emotion e ON pe.EmotionId = e.Id;
+
+--SELECT * FROM EMOTION; 
+
+--SELECT COUNT(*) as 'Number of Poems'
+--FROM Poem;
+
+--SELECT a.Name as Author, g.Name as Grade, gen.Name as Gender
+--FROM Author a
+--	LEFT JOIN Grade g
+--	ON a.GradeId = g.Id
+--	LEFT JOIN Gender gen
+--	ON a.GenderId = gen.Id
+--WHERE a.Id <= 76
+--ORDER BY a.Name;
+
+--SELECT COUNT(WordCount) as 'Total Poems Words'
+--FROM Poem;
+
+
